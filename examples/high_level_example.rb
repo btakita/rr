50485c36f2dab5a3627ab90e4834704803ba23f5
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2007-06-17 08:13:27 +0000

    Isolated remock and restub examples.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/rr/trunk@86 af276e61-6b34-4dac-905b-574b5f35ef33

--- examples/high_level_example.rb
+++ examples/high_level_example.rb
@@ -10,19 +10,26 @@ describe "RR existing object inline interactions" do
   end
 
   after(:each) do
-    RR::Space.instance.verify_doubles
+    RR::Space.instance.reset_doubles
   end
 
-  it "mocks" #do
+  it "mocks" do
+    obj = @obj
+
+    # TODO: BT - Remove this block when rspec support is added to RR.
+    # We do this to avoid a conflict with Rspec's mock method.
+    Object.new.instance_eval do
+      mock(obj).to_s {"a value"}
+    end
+    @obj.to_s.should == "a value"
+    proc {@obj.to_s}.should raise_error(RR::Expectations::TimesCalledExpectationError)
+  end
+
+  it "remocks" #do
 #    obj = @obj
-#
-#    # TODO: BT - Remove this block when rspec support is added to RR.
-#    # We do this to avoid a conflict with Rspec's mock method.
 #    Object.new.instance_eval do
 #      mock(obj).to_s {"a value"}
 #    end
-#    @obj.to_s.should == "a value"
-#    proc {@obj.to_s}.should raise_error(RR::Expectations::TimesCalledExpectationError)
 #
 #    Object.new.instance_eval do
 #      mock(obj).to_s {"a value"}.twice
@@ -32,11 +39,16 @@ describe "RR existing object inline interactions" do
 #    proc {@obj.to_s}.should raise_error(RR::Expectations::TimesCalledExpectationError)
 #  end
 
-  it "probes" #do
+  it "probes" do
+    expected_to_s_value = @obj.to_s
+    probe(@obj).to_s
+    @obj.to_s.should == expected_to_s_value
+    proc {@obj.to_s}.should raise_error
+  end
+
+  it "reprobes" #do
 #    expected_to_s_value = @obj.to_s
 #    probe(@obj).to_s
-#    @obj.to_s.should == expected_to_s_value
-#    proc {@obj.to_s}.should raise_error
 #
 #    probe(@obj).to_s.twice
 #    @obj.to_s.should == expected_to_s_value
