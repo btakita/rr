a6ac547604c224d694b839ca83ed803331944967
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2007-07-09 00:24:48 +0000

    Added example that ensures the backtrace is tweaked for RR in rspec.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/rr/trunk@216 af276e61-6b34-4dac-905b-574b5f35ef33

--- examples/rr/rspec/rspec_usage_example.rb
+++ examples/rr/rspec/rspec_usage_example.rb
@@ -1,29 +1,6 @@
 dir = File.dirname(__FILE__)
 require "#{dir}/../../example_helper"
 
-describe RR, " backtrace tweaking" do
-  it "hides rr library from the backtrace by default" do
-    output = StringIO.new("")
-    backtrace_tweaker = ::Spec::Runner::QuietBacktraceTweaker.new
-    formatter = ::Spec::Runner::Formatter::BaseTextFormatter.new(output)
-    reporter = ::Spec::Runner::Reporter.new([formatter], backtrace_tweaker)
-
-    behaviour = ::Spec::DSL::Behaviour.new("example") {}
-    subject = @subject
-    behaviour.it("hides RR framework in backtrace") do
-      mock(subject).foobar()
-      RR::Space::instance.verify_double(subject, :foobar)
-    end
-
-    reporter.add_behaviour(behaviour)
-
-    behaviour.run(reporter)
-    reporter.dump
-
-    output.string.should_not include("lib/rr")
-  end  
-end
-
 describe RR, "#mock" do
   before do
     @subject = Object.new
