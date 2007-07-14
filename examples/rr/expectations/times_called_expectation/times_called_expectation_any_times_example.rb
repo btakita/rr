830ca17b6b303ff2c64b07984a8514b51186ea9a
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2007-07-14 19:59:30 +0000

    Added example.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/rr/trunk@307 af276e61-6b34-4dac-905b-574b5f35ef33

---
+++ examples/rr/expectations/times_called_expectation/times_called_expectation_any_times_example.rb
@@ -0,0 +1,43 @@
+require "examples/example_helper"
+
+module RR
+module Expectations
+  describe TimesCalledExpectation, ' with AnyTimesMatcher', :shared => true do
+    it_should_behave_like "RR::Expectations::TimesCalledExpectation"
+    
+    before do
+      @times = 3
+      @at_least = TimesCalledMatchers::AnyTimesMatcher.new(@times)
+      @expectation = TimesCalledExpectation.new(@at_least)
+    end
+  end
+
+  describe TimesCalledExpectation, "#verify! with AnyTimesMatcher" do
+    it_should_behave_like "RR::Expectations::TimesCalledExpectation with AnyTimesMatcher"
+
+    it "always passes" do
+      @expectation.verify!
+      10.times {@expectation.attempt!}
+      @expectation.verify!
+    end
+  end
+
+  describe TimesCalledExpectation, "#attempt? with AnyTimesMatcher" do
+    it_should_behave_like "RR::Expectations::TimesCalledExpectation with AnyTimesMatcher"
+
+    it "always returns true" do
+      @expectation.should be_attempt
+      10.times {@expectation.attempt!}
+      @expectation.should be_attempt
+    end
+  end
+
+  describe TimesCalledExpectation, "#attempt! with AnyTimesMatcher" do
+    it_should_behave_like "RR::Expectations::TimesCalledExpectation with AnyTimesMatcher"
+    
+    it "always passes" do
+      10.times {@expectation.attempt!}
+    end
+  end
+end
+end
