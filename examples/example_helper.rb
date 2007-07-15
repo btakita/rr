48a18c7b7dad8c20635cfb69e470a26d5bebe7f7
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2007-07-15 07:10:46 +0000

    Organizing examples.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/rr/trunk@330 af276e61-6b34-4dac-905b-574b5f35ef33

--- examples/example_helper.rb
+++ examples/example_helper.rb
@@ -1,6 +1,6 @@
 dir = File.dirname(__FILE__)
 require "#{dir}/environment_fixture_setup"
-require "examples/rr/space_helper"
+require "examples/rr/space/space_helper"
 require "examples/rr/expectations/times_called_expectation/times_called_expectation_helper"
 require "examples/rr/extensions/double_methods_example_helper"
 
