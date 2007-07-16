740d29a9a09865638b5c52935c9cc4170565b4e9
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2007-07-16 02:01:52 +0000

    RDoc changes.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/rr/trunk@338 af276e61-6b34-4dac-905b-574b5f35ef33

--- lib/rr/times_called_matchers/non_terminal.rb
+++ lib/rr/times_called_matchers/non_terminal.rb
@@ -1,11 +1,12 @@
 module RR
 module TimesCalledMatchers
   # Including this module marks the TimesCalledMatcher as NonTerminal.
-  # Being NonTerminal means the attempt? method will always return
-  # true.
+  # Being NonTerminal means the Scenario will not "terminate" even when
+  # called infinite times.
   #
   # The Scenario that uses a NonTerminal TimesCalledMatcher will
   # continue using the Scenario when passed the matching arguments.
+  # This is done by the attempt? always returning true.
   #
   # This is in opposition to Terminal TimesCalledMatchers, where
   # attempt? will eventually return false.
--- lib/rr/times_called_matchers/terminal.rb
+++ lib/rr/times_called_matchers/terminal.rb
@@ -1,12 +1,13 @@
 module RR
 module TimesCalledMatchers
   # Including this module marks the TimesCalledMatcher as Terminal.
-  # Being Terminal means the attempt? method will eventually return
-  # false.
+  # Being Terminal the Scenario will "terminate" when times called is
+  # finite.
   #
   # The Scenario that uses a Terminal TimesCalledMatcher will
   # eventually be passed over to the next Scenario when passed
-  # the matching arguments enough times.
+  # the matching arguments enough times. This is done by the attempt?
+  # method returning false when executed a finite number of times.
   #
   # This is in opposition to NonTerminal TimesCalledMatchers, where
   # attempt? will always return true.
