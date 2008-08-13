c576165066ea19d421cba37e03c8652e893431d7
Brian Takita <brian.takita@gmail.com>
2008-08-13 00:38:41 -0700

    - Fix conflict with Mocha overriding Object#verify

--- lib/rr.rb
+++ lib/rr.rb
@@ -48,8 +48,17 @@ require "#{dir}/rr/adapters/test_unit"
 
 module RR
   class << self
+    (RR::Space.instance_methods - Object.instance_methods).each do |method_name|
+      returns_method = <<-METHOD
+        def #{method_name}(*args, &block)
+          RR::Space.instance.__send__(:#{method_name}, *args, &block)
+        end
+      METHOD
+      class_eval(returns_method, __FILE__, __LINE__ - 4)
+    end
+
     def method_missing(method_name, *args, &block)
       RR::Space.instance.__send__(method_name, *args, &block)
     end
-  end  
+  end
 end
