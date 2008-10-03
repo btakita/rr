2cf0b6b7888cc3b3bbf260258256ee4d225d015f
Brian Takita <brian.takita@gmail.com>
2008-10-02 19:51:43 -0700

    Moved instance_of and proxy strategy implementation into Builder.

--- lib/rr/double_definitions/builders/builder.rb
+++ lib/rr/double_definitions/builders/builder.rb
@@ -2,38 +2,77 @@ module RR
   module DoubleDefinitions
     module Builders
       class Builder #:nodoc:
-        attr_reader :definition, :args, :handler, :core_strategy
+        attr_reader :creator, :subject, :method_name, :args, :handler, :definition, :core_strategy
         include Errors
+        include Space::Reader
 
-        def initialize
-          @is_using_proxy_strategy = false
+        def initialize(creator)
+          @creator = creator
+          @using_proxy_strategy = false
+          @using_instance_of_strategy = nil
           @core_strategy = nil
         end
 
-        def build(definition, args, handler)
-          @definition, @args, @handler = definition, args, handler
+        def build(subject, method_name, args, handler)
+          @subject, @method_name, @args, @handler = subject, method_name, args, handler
+          setup_double
           verify_strategy
           send(@core_strategy)
-          is_using_proxy_strategy?? proxy : reimplementation
+          using_proxy_strategy?? proxy : reimplementation
           @definition
         end
 
         def set_core_strategy(strategy)
           verify_no_core_strategy
           @core_strategy = strategy
-          proxy_when_dont_allow_error if strategy == :dont_allow && @is_using_proxy_strategy
+          proxy_when_dont_allow_error if strategy == :dont_allow && @using_proxy_strategy
         end
 
-        def using_proxy_strategy
+        def use_proxy_strategy
           proxy_when_dont_allow_error if @core_strategy == :dont_allow
-          @is_using_proxy_strategy = true
+          @using_proxy_strategy = true
         end
 
-        def is_using_proxy_strategy?
-          !!@is_using_proxy_strategy
+        def using_proxy_strategy?
+          !!@using_proxy_strategy
+        end
+
+        def use_instance_of_strategy
+          @using_instance_of_strategy = true
+        end
+
+        def using_instance_of_strategy?
+          !!@using_instance_of_strategy
         end
 
         protected
+        def setup_double
+          @definition = DoubleDefinition.new(creator, subject)
+          if using_instance_of_strategy?
+            setup_doubles_instances_of_subject(method_name)
+          else
+            setup_double_for_subject(method_name)
+          end
+        end
+
+        def setup_double_for_subject(method_name)
+          double_injection = space.double_injection(subject, method_name)
+          Double.new(double_injection, definition)
+        end
+
+        def setup_doubles_instances_of_subject(instance_method_name)
+          class_handler = lambda do |return_value|
+            double_injection = space.double_injection(return_value, instance_method_name)
+            Double.new(double_injection, definition)
+            return_value
+          end
+
+          instance_of_subject_builder = Builders::Builder.new(creator)
+          instance_of_subject_builder.set_core_strategy(:stub)
+          instance_of_subject_builder.use_proxy_strategy
+          instance_of_subject_builder.build(subject, :new, [], class_handler)
+        end        
+
         def mock
           @definition.with(*@args).once
         end
--- lib/rr/double_definitions/double_definition_creator.rb
+++ lib/rr/double_definitions/double_definition_creator.rb
@@ -8,9 +8,7 @@ module RR
 
       def initialize
         @core_strategy = nil
-        @using_proxy_strategy = false
-        @using_instance_of_strategy = nil
-        @builder = Builders::Builder.new
+        @builder = Builders::Builder.new(self)
       end
 
       def mock(subject=NO_SUBJECT, method_name=nil, &definition_eval_block) # :nodoc
@@ -36,7 +34,7 @@ module RR
 
       def proxy(subject=NO_SUBJECT, method_name=nil, &definition_eval_block) # :nodoc
         add_strategy(subject, method_name, definition_eval_block) do
-          builder.using_proxy_strategy
+          builder.use_proxy_strategy
         end
       end
       alias_method :probe, :proxy
@@ -46,21 +44,13 @@ module RR
           raise ArgumentError, "instance_of only accepts class objects" unless subject.is_a?(Class)
         end
         add_strategy(subject, method_name, definition_eval_block) do
-          @using_instance_of_strategy = true
+          builder.use_instance_of_strategy
           return self if subject === NO_SUBJECT
         end
       end
 
       def create(subject, method_name, *args, &handler)
-        @args = args
-        @handler = handler
-        if @using_instance_of_strategy
-          setup_doubles_for_class_instances(subject, method_name)
-        else
-          setup_double(subject, method_name)
-        end
-        builder.build(@definition, @args, @handler)
-        @definition
+        builder.build(subject, method_name, args, handler)
       end
 
       protected
@@ -80,31 +70,6 @@ module RR
 
       def no_subject?(subject)
         subject.__id__ === NO_SUBJECT.__id__
-      end
-
-      def setup_double(subject, method_name)
-        @double_injection = space.double_injection(subject, method_name)
-        @double = Double.new(@double_injection, DoubleDefinition.new(self, subject))
-        @definition = @double.definition
-      end
-
-      def setup_doubles_for_class_instances(subject, method_name)
-        class_double = space.double_injection(subject, :new)
-        class_double = Double.new(class_double, DoubleDefinition.new(self, subject))
-
-        instance_method_name = method_name
-
-        @definition = DoubleDefinition.new(self, subject)
-        class_handler = lambda do |return_value|
-          double_injection = space.double_injection(return_value, instance_method_name)
-          Double.new(double_injection, @definition)
-          return_value
-        end
-
-        builder = Builders::Builder.new
-        builder.set_core_strategy(:stub)
-        builder.using_proxy_strategy
-        builder.build(class_double.definition, [], class_handler)
       end
     end
   end
