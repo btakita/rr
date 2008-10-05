a67687ac3da5f292ca34b340a8fa1056ebac3d25
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-10-05 00:55:06 +0000

    Reformatting.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/rr/trunk@1867 af276e61-6b34-4dac-905b-574b5f35ef33

--- lib/rr/double_definitions/double_definition_creator.rb
+++ lib/rr/double_definitions/double_definition_creator.rb
@@ -60,7 +60,7 @@ module RR
         if no_subject?(subject)
           self
         elsif method_name
-          create subject, method_name, &definition_eval_block
+          create(subject, method_name, &definition_eval_block)
         else
           DoubleDefinitionCreatorProxy.new(self, subject, &definition_eval_block)
         end
