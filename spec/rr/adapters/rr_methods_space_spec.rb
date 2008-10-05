57fac2a6c363fe7657ae7d30cc7dbfa886f27259
btakita <btakita@af276e61-6b34-4dac-905b-574b5f35ef33>
2008-10-05 18:14:28 +0000

    Removed unnecessary describe.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/pivotalrb/rr/trunk@1890 af276e61-6b34-4dac-905b-574b5f35ef33

--- spec/rr/adapters/rr_methods_space_spec.rb
+++ spec/rr/adapters/rr_methods_space_spec.rb
@@ -3,125 +3,123 @@ require File.expand_path("#{File.dirname(__FILE__)}/../../spec_helper")
 module RR
   module Adapters
     describe RRMethods do
-      describe "Space interactions" do
-        attr_reader :space, :subject_1, :subject_2, :method_name
-        it_should_behave_like "Swapped Space"
-        it_should_behave_like "RR::Adapters::RRMethods"
-        before do
-          @subject_1 = Object.new
-          @subject_2 = Object.new
-          @method_name = :foobar
+      attr_reader :space, :subject_1, :subject_2, :method_name
+      it_should_behave_like "Swapped Space"
+      it_should_behave_like "RR::Adapters::RRMethods"
+      before do
+        @subject_1 = Object.new
+        @subject_2 = Object.new
+        @method_name = :foobar
+      end
+
+      describe "#verify" do
+        it "#verify verifies and deletes the double_injections" do
+          verifies_all_double_injections {verify}
         end
 
-        describe "#verify" do
-          it "#verify verifies and deletes the double_injections" do
-            verifies_all_double_injections {verify}
-          end
-
-          it "#rr_verify verifies and deletes the double_injections" do
-            verifies_all_double_injections {rr_verify}
-          end
-
-          def verifies_all_double_injections
-            double_1 = space.double_injection(subject_1, method_name)
-            double_1_verify_calls = 0
-            double_1_reset_calls = 0
-            (
-            class << double_1;
-              self;
-            end).class_eval do
-              define_method(:verify) do ||
-                double_1_verify_calls += 1
-              end
-              define_method(:reset) do ||
-                double_1_reset_calls += 1
-              end
-            end
-            double_2 = space.double_injection(subject_2, method_name)
-            double_2_verify_calls = 0
-            double_2_reset_calls = 0
-            (
-            class << double_2;
-              self;
-            end).class_eval do
-              define_method(:verify) do ||
-                double_2_verify_calls += 1
-              end
-              define_method(:reset) do ||
-                double_2_reset_calls += 1
-              end
-            end
-
-            yield
-            double_1_verify_calls.should == 1
-            double_2_verify_calls.should == 1
-            double_1_reset_calls.should == 1
-            double_1_reset_calls.should == 1
-          end
+        it "#rr_verify verifies and deletes the double_injections" do
+          verifies_all_double_injections {rr_verify}
         end
 
-        describe "#reset" do
-          it "#reset removes the ordered doubles" do
-            removes_ordered_doubles {reset}
-          end
-
-          it "#rr_reset removes the ordered doubles" do
-            removes_ordered_doubles {rr_reset}
-          end
-
-          it "#reset resets all double_injections" do
-            resets_all_double_injections {reset}
-          end
-
-          it "#rr_reset resets all double_injections" do
-            resets_all_double_injections {rr_reset}
-          end
-
-          def removes_ordered_doubles
-            double_1 = new_double(
-              space.double_injection(subject_1, :foobar1),
-              RR::DoubleDefinitions::DoubleDefinition.new(creator = RR::DoubleDefinitions::DoubleDefinitionCreator.new, subject_1)
-            )
-            double_2 = new_double(
-              space.double_injection(subject_2, :foobar2),
-              RR::DoubleDefinitions::DoubleDefinition.new(creator = RR::DoubleDefinitions::DoubleDefinitionCreator.new, subject_2)
-            )
-
-            double_1.ordered
-            double_2.ordered
-
-            space.ordered_doubles.should_not be_empty
-
-            yield
-            space.ordered_doubles.should be_empty
-          end
-
-          def resets_all_double_injections
-            double_1 = space.double_injection(subject_1, method_name)
-            double_1_reset_calls = 0
-            (
-            class << double_1;
-              self;
-            end).class_eval do
-              define_method(:reset) do ||
-                double_1_reset_calls += 1
-              end
+        def verifies_all_double_injections
+          double_1 = space.double_injection(subject_1, method_name)
+          double_1_verify_calls = 0
+          double_1_reset_calls = 0
+          (
+          class << double_1;
+            self;
+          end).class_eval do
+            define_method(:verify) do ||
+              double_1_verify_calls += 1
             end
-            double_2 = space.double_injection(subject_2, method_name)
-            double_2_reset_calls = 0
-            (
-            class << double_2;
-              self;
-            end).class_eval do
-              define_method(:reset) do ||
-                double_2_reset_calls += 1
-              end
+            define_method(:reset) do ||
+              double_1_reset_calls += 1
             end
-
-            yield
-            double_1_reset_calls.should == 1
-            double_2_reset_calls.should == 1
           end
+          double_2 = space.double_injection(subject_2, method_name)
+          double_2_verify_calls = 0
+          double_2_reset_calls = 0
+          (
+          class << double_2;
+            self;
+          end).class_eval do
+            define_method(:verify) do ||
+              double_2_verify_calls += 1
+            end
+            define_method(:reset) do ||
+              double_2_reset_calls += 1
+            end
+          end
+
+          yield
+          double_1_verify_calls.should == 1
+          double_2_verify_calls.should == 1
+          double_1_reset_calls.should == 1
+          double_1_reset_calls.should == 1
+        end
+      end
+
+      describe "#reset" do
+        it "#reset removes the ordered doubles" do
+          removes_ordered_doubles {reset}
+        end
+
+        it "#rr_reset removes the ordered doubles" do
+          removes_ordered_doubles {rr_reset}
+        end
+
+        it "#reset resets all double_injections" do
+          resets_all_double_injections {reset}
+        end
+
+        it "#rr_reset resets all double_injections" do
+          resets_all_double_injections {rr_reset}
+        end
+
+        def removes_ordered_doubles
+          double_1 = new_double(
+            space.double_injection(subject_1, :foobar1),
+            RR::DoubleDefinitions::DoubleDefinition.new(creator = RR::DoubleDefinitions::DoubleDefinitionCreator.new, subject_1)
+          )
+          double_2 = new_double(
+            space.double_injection(subject_2, :foobar2),
+            RR::DoubleDefinitions::DoubleDefinition.new(creator = RR::DoubleDefinitions::DoubleDefinitionCreator.new, subject_2)
+          )
+
+          double_1.ordered
+          double_2.ordered
+
+          space.ordered_doubles.should_not be_empty
+
+          yield
+          space.ordered_doubles.should be_empty
+        end
+
+        def resets_all_double_injections
+          double_1 = space.double_injection(subject_1, method_name)
+          double_1_reset_calls = 0
+          (
+          class << double_1;
+            self;
+          end).class_eval do
+            define_method(:reset) do ||
+              double_1_reset_calls += 1
+            end
+          end
+          double_2 = space.double_injection(subject_2, method_name)
+          double_2_reset_calls = 0
+          (
+          class << double_2;
+            self;
+          end).class_eval do
+            define_method(:reset) do ||
+              double_2_reset_calls += 1
+            end
+          end
+
+          yield
+          double_1_reset_calls.should == 1
+          double_2_reset_calls.should == 1
         end
       end
     end
