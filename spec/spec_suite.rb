5265af05e4114b5c482f322abf0117748901c9b1
Brian Takita <brian@honk.com>
2009-10-18 22:29:32 -0700

    Running suite while ignoring 'warning: useless use of' warnings

--- spec/spec_suite.rb
+++ spec/spec_suite.rb
@@ -1,4 +1,12 @@
+require "rubygems"
+require "session"
+
 class ExampleSuite
+  attr_reader :bash
+  def initialize
+    @bash = Session::Bash.new
+  end
+
   def run
     run_core_examples
     run_rspec_examples
@@ -6,15 +14,24 @@ class ExampleSuite
   end
 
   def run_core_examples
-    system("ruby -W #{dir}/core_spec_suite.rb #{spec_opts}") || raise("Core suite Failed")
+    run_suite("#{dir}/core_spec_suite.rb #{spec_opts}") || raise("Core suite Failed")
   end
 
   def run_rspec_examples
-    system("ruby -W #{dir}/rspec_spec_suite.rb #{spec_opts}") || raise("Rspec suite Failed")
+    run_suite("#{dir}/rspec_spec_suite.rb #{spec_opts}") || raise("Rspec suite Failed")
   end
 
   def run_test_unit_examples
-    system("ruby -W #{dir}/test_unit_spec_suite.rb") || raise("Test::Unit suite Failed")
+    run_suite("#{dir}/test_unit_spec_suite.rb") || raise("Test::Unit suite Failed")
+  end
+
+  def run_suite(path)
+    # From http://www.eglug.org/node/946
+    bash.execute "exec 3>&1", :out => STDOUT, :err => STDERR
+    bash.execute "ruby -W #{path} 2>&1 >&3 3>&- | grep -v 'warning: useless use of' 3>&-; STATUS=${PIPESTATUS[0]}", :out => STDOUT, :err => STDERR
+    status = bash.execute("echo $STATUS")[0].to_s.strip.to_i
+    bash.execute "exec 3>&-", :out => STDOUT, :err => STDERR
+    return status == 0
   end
 
   def spec_opts
