59fb566f2cc89d747da41ca5845c5e42bf194eed
Brian Takita <brian@honk.com>
2010-03-06 17:08:58 -0800

    Added specs for new_instance_of.

---
+++ spec/api/new_instance_of/new_instance_of_spec.rb
@@ -0,0 +1,15 @@
+require File.expand_path("#{File.dirname(__FILE__)}/../../spec_helper")
+
+describe "new_instance_of" do
+  it "applies to instances instantiated before the Double expection was created" do
+    subject_class = Class.new
+    existing_subject = subject_class.new
+    stub.new_instance_of(subject_class) do |o|
+      o.to_s {"Subject is stubbed"}
+    end
+    new_subject = subject_class.new
+
+    existing_subject.to_s.should_not == "Subject is stubbed"
+    new_subject.to_s.should == "Subject is stubbed"
+  end
+end
