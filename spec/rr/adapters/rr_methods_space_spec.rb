29ef8b6cc65b6b594cab46552af614c3ffd511b7
Ben Woosley <ben.woosley@gmail.com>
2012-02-01 12:00:12 -0800

    Rename utility methods reset and verify to reset_rr and verify_rr to avoid clashes with other methods in the cucumber World namespace, e.g. Gherkin::Formatter::AnsiEscapes#reset

--- spec/rr/adapters/rr_methods_space_spec.rb
+++ spec/rr/adapters/rr_methods_space_spec.rb
@@ -12,13 +12,13 @@ module RR
         @method_name = :foobar
       end
 
-      describe "#verify" do
-        it "aliases #rr_verify" do
-          RRMethods.instance_method("verify").should == RRMethods.instance_method("rr_verify")
+      describe "#verify_rr" do
+        it "aliases #rr_verify_rr" do
+          RRMethods.instance_method("verify_rr").should == RRMethods.instance_method("rr_verify_rr")
         end
       end
 
-      describe "#rr_verify" do
+      describe "#rr_verify_rr" do
         it "verifies and deletes the double_injections" do
           double_1 = ::RR::Injections::DoubleInjection.find_or_create_by_subject(subject_1, method_name)
           double_1_verify_calls = 0
@@ -46,7 +46,7 @@ module RR
             end
           end
 
-          rr_verify
+          rr_verify_rr
           double_1_verify_calls.should == 1
           double_2_verify_calls.should == 1
           double_1_reset_calls.should == 1
@@ -54,20 +54,20 @@ module RR
         end
       end
 
-      describe "#reset" do
-        it "aliases #rr_reset" do
-          RRMethods.instance_method("reset").should == RRMethods.instance_method("rr_reset")
+      describe "#reset_rr" do
+        it "aliases #rr_reset_rr" do
+          RRMethods.instance_method("reset_rr").should == RRMethods.instance_method("rr_reset_rr")
         end
       end
 
-      describe "#rr_reset" do
+      describe "#rr_reset_rr" do
         it "removes the ordered doubles" do
           mock(subject_1).foobar1.ordered
           mock(subject_2).foobar2.ordered
 
           ::RR::Injections::DoubleInjection.instances.should_not be_empty
 
-          rr_reset
+          rr_reset_rr
           ::RR::Injections::DoubleInjection.instances
           ::RR::Injections::DoubleInjection.instances.should be_empty
         end
@@ -88,7 +88,7 @@ module RR
             end
           end
 
-          rr_reset
+          rr_reset_rr
           double_1_reset_calls.should == 1
           double_2_reset_calls.should == 1
         end
