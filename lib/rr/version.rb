547495aeb8c5f8d419bec7a5ec85806d329c54fc
Elliot Winkler <elliot.winkler@gmail.com>
2013-03-28 19:20:55 -0600

    Fix RR.version to remove trailing new line character

--- lib/rr/version.rb
+++ lib/rr/version.rb
@@ -1,5 +1,5 @@
 
 module RR
-  VERSION = File.read(File.expand_path('../../../VERSION', __FILE__))
+  VERSION = File.read(File.expand_path('../../../VERSION', __FILE__)).strip
   def self.version; VERSION; end
 end
