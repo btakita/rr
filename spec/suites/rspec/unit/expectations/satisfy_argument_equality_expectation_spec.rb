bc5aa0c30c84a782fc182ef45e3a7fe6a29fb8fa
Elliot Winkler <elliot.winkler@gmail.com>
2013-03-28 19:52:53 -0600

    Fix whitespace

--- spec/suites/rspec/unit/expectations/satisfy_argument_equality_expectation_spec.rb
+++ spec/suites/rspec/unit/expectations/satisfy_argument_equality_expectation_spec.rb
@@ -4,22 +4,22 @@ module RR
 module Expectations
   describe ArgumentEqualityExpectation, "with Satisfy argument" do
     attr_reader :expectation, :expectation_proc, :expected_value, :satisfy_matcher
-    
+
     before do
       @expected_value = :foo
       @expectation_proc = lambda {|argument| argument == expected_value}
       @satisfy_matcher = satisfy(&expectation_proc)
       @expectation = ArgumentEqualityExpectation.new(satisfy_matcher)
     end
-    
+
     describe "#exact_match?" do
       before do
       end
-      
+
       it "returns true when passed a Satisfy matcher with the same proc" do
         expect(expectation).to be_exact_match(RR::WildcardMatchers::Satisfy.new(expectation_proc))
       end
-      
+
       it "returns false when passed a Satisfy matcher with another proc" do
         expectation.should_not be_exact_match(RR::WildcardMatchers::Satisfy.new(lambda {}))
       end
@@ -39,12 +39,12 @@ module Expectations
         expect((!!expectation_proc.call(expected_value))).to be_true
         expect(expectation).to be_wildcard_match(expected_value)
       end
-      
+
       it "returns false when the proc returns a falsey value" do
         expect((!!expectation_proc.call(:bar))).to be_false
         expectation.should_not be_wildcard_match(:bar)
       end
-      
+
       it "returns true when an exact match" do
         expect(expectation).to be_wildcard_match(satisfy_matcher)
       end
